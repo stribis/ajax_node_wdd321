1fad2565b4aeb023c3a1a5535f9c45b10e97372f
Martin Hutchings <martin.hutchings@sae.edu>
2022-08-18 15:05:34 +0200

    edit amiibo branch

--- 01_ajax/07_php_ajax_simple/code.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.do').addEventListener('click', getData)
-
-async function getData() {
-
-  const response = await fetch('send_data.php')
-  const data = await response.text()
-
-  console.log(data)
-}
